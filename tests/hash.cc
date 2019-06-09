be3bdf56763be508b7948f4c803e0ac34993da80
Philip Trettner <philip.trettner@rwth-aachen.de>
2019-06-09 17:19:25 +0200

    second hash test with specialization directly in std

--- tests/hash.cc
+++ tests/hash.cc
@@ -12,10 +12,27 @@ struct stdfwd::hash<foo>
     size_t operator()(foo const& f) const noexcept { return f.a; }
 };
 
+struct bar
+{
+    int a;
+
+    bool operator==(bar const& r) const { return a == r.a; }
+};
+template <>
+struct std::hash<bar>
+{
+    size_t operator()(bar const& f) const noexcept { return f.a; }
+};
+
 #include <unordered_map>
 
 int hash_test(foo f)
 {
     std::unordered_map<foo, int> m;
+    return m[f];
+}
+int hash_test(bar f)
+{
+    std::unordered_map<bar, int> m;
     return m[f];
 }
